f63d3769cf023fae9734f97ccfbc284916dd047c
Jack Schwarz <jack@JacksMacStudio.lan>
2026-05-29 22:22:31 -0500

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   // For username.github.io repos, base is '/'.
   // For project pages (username.github.io/repo-name), set base to '/repo-name/'.
-  base: '/',
+  base: './',
 })
