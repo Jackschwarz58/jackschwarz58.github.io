a880eb5fe93b2e75e7a11b66a232287399fed27c
Jack Schwarz <jackschwarz58@gmail.com>
2026-07-27 21:29:00 -0500

    Temporarily remove podcasts

--- src/pages/Media.tsx
+++ src/pages/Media.tsx
@@ -4,37 +4,6 @@ export default function Media() {
   return (
     <Stack gap="lg" style={{ margin: '1.5rem' }}>
       <Title order={2}>Podcasts</Title>
-      <SimpleGrid cols={{ base: 1, lg: 3 }}>
-        <Stack gap="xs">
-          <Text size="xl">2026</Text>
-          <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay; encrypted-media"
-                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A2267767553%3Fsecret_token%3Ds-KUerzr4Nh7h&color=%23c1c1c5&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" />
-        </Stack>
-        <Stack gap="xs">
-          <Text size="xl">2025</Text>
-          <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay; encrypted-media"
-                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A2135445018%3Fsecret_token%3Ds-s3p3zXJHqb1&color=%23c1c1c5&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" />
-          <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay; encrypted-media"
-                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A2043892564%3Fsecret_token%3Ds-3OpBHu1Sncg&color=%23c1c1c5&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" />
-        </Stack>
-        <Stack gap="xs">
-          <Text size="xl">2024</Text>
-          <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay; encrypted-media"
-                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A1867589301%3Fsecret_token%3Ds-TlaBA0B5zNc&color=%23c1c1c5&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" />
-          <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay; encrypted-media"
-                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A1739915022%3Fsecret_token%3Ds-aK6ZHCt2NL1&color=%23c1c1c5&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" />
-        </Stack>
-        <Stack gap="xs">
-          <Text size="xl">2023</Text>
-          <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay; encrypted-media"
-                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A1566518137%3Fsecret_token%3Ds-AsnH3cFZp8M&color=%23c1c1c5&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" />
-        </Stack>
-        <Stack gap="xs">
-          <Text size="xl">2022</Text>
-          <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay; encrypted-media"
-                  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A1305278596%3Fsecret_token%3Ds-kYazJa81wnj&color=%23c1c1c5&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" />
-        </Stack>
-      </SimpleGrid>
       <Title order={2}>Videos</Title>
       <SimpleGrid cols={{ base: 1, lg: 3 }}>
         <Stack gap="xs">
